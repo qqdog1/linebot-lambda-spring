1e9fa6f40c681a70f7bcecfacec8ff18a4805a69
Shawn <qqdog1@yahoo.com.tw>
2020-12-18 18:43:28 +0800

    1. clean up

--- src/main/java/name/qd/linebot/lambda/controller/TestController.java
+++ src/main/java/name/qd/linebot/lambda/controller/TestController.java
@@ -2,7 +2,6 @@ package name.qd.linebot.lambda.controller;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import javax.annotation.PostConstruct;
@@ -10,8 +9,6 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestBody;
